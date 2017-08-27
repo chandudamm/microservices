a2b189d4f47f4a5cd9dab0e01566c9c8c9c04c61
lakshmi <laxmi.nrt@gmail.com>
2017-08-27 16:00:23 +0530

    spring cloud starter task related code added

--- Customer/src/main/java/com/example/customer/CustomerApplication.java
+++ Customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -1,12 +1,31 @@
 package com.example.customer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class CustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
+	
+	@Bean
+	CustomerTask customerTask() {
+		return new CustomerTask();
+	}
+	
+	public class CustomerTask implements CommandLineRunner{
+
+		@Override
+		public void run(String... arg0) throws Exception {
+			System.out.println("Args length:"+arg0.length);
+			System.out.println(arg0[0]+"\n"+arg0[1]+"\n"+arg0[2]);
+		}
+	}
+
 }
--- Customer/src/main/java/com/example/customer/controller/CustomerController.java
+++ Customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -1,12 +1,13 @@
 package com.example.customer.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +16,31 @@ import com.example.customer.domain.Customer;
 import com.example.customer.repository.CustomerRepository;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/customers")
 public class CustomerController {
 	
 	@Autowired
 	CustomerRepository customerRepository;
 	
-	@RequestMapping(path="/customers", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping
 	public List<Customer> getCustomers(){
 		return customerRepository.findAll();
 	}
 	
-	@RequestMapping(path="/customers/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	/*@GetMapping(path="/{id}")
 	public Customer findCustomer(@PathVariable("id") long customerId){
 		return customerRepository.findOne(customerId);
+	}*/
+	
+	@GetMapping(path="/{name}")
+	public Customer findByName(@PathVariable("name") String name ){
+		return customerRepository.findByName(name);
+	}
+	
+	@PostMapping
+	public Customer createCustomer(@RequestBody Customer customer){
+		customerRepository.save(customer);
+		return customerRepository.findByName(customer.getName());
 	}
 
 }
